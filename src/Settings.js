f3487ae8966ce02ea6e68c42827bbf56782e3084
Pedro Valdez <79939920+pedro-valdez@users.noreply.github.com>
2025-09-05 21:31:43 -0500

    feat(ui): Add Settings class
    Class adds reactivity to values meant to change in settings modal.

---
+++ src/Settings.js
@@ -0,0 +1,47 @@
+export default class Settings {
+    static #settings = {}
+
+    static ensure(key, cb, fallbackValue) {
+        const hasKey = Settings.#settings.hasOwnProperty(key)
+
+        if (!hasKey) {
+            Settings.#settings[key] = {
+                value: fallbackValue,
+                callbacks: [cb],
+            }
+
+            return fallbackValue
+        }
+
+        const setting = Settings.#settings[key]
+        const hasValue = 'value' in setting
+        const hasCallbackArray = Array.isArray(setting.callbacks)
+
+        if (!hasValue) {
+            setting.value = fallbackValue
+        }
+
+        if (!hasCallbackArray) {
+            setting.callbacks = []
+        }
+
+        if (cb && !setting.callbacks.includes(cb)) {
+            setting.callbacks.push(cb)
+        }
+
+        return setting.value
+    }
+
+    static change(key, value) {
+        const setting = Settings.#settings[key]
+
+        if (setting === undefined) {
+            throw new Error(
+                `${Settings.change.name} called with nonexistent key: ${key}`
+            )
+        }
+
+        setting.value = value
+        setting.callbacks.forEach((cb) => cb(value))
+    }
+}
